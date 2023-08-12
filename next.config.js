ea8dfd2f6b09841e5965b04e51ae0c4e5de17cc6
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2023-08-11 20:14:37 -0700

    feat: add lighthouse CI

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   experimental: {
     typedRoutes: true,
     serverActions: true,
   },
-};
+});
--- src/components/Misc/AppProvider.tsx
+++ src/components/Misc/AppProvider.tsx
@@ -2,18 +2,22 @@
 
 import React from "react";
 import { CacheProvider } from "@chakra-ui/next-js";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, cookieStorageManagerSSR } from "@chakra-ui/react";
 
 import { theme } from "@/utils";
+
+const COLOR_MODE_COOKIE = "chakra_color_mode";
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
 
 const AppProvider = ({ children }: AppProviderProps) => {
+  const cookieStorageManager = cookieStorageManagerSSR(COLOR_MODE_COOKIE);
+
   return (
     <CacheProvider>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider theme={theme} colorModeManager={cookieStorageManager}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         {children}
       </ChakraProvider>
