310432e44b81e6006702e9248d35777dd26331f0
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-01-18 22:24:58 -0800

    chore: add CustomParseFormat dayjs plugin
    - add CustomParseFormat dayjs plugin

--- apps/client/src/utils/date.ts
+++ apps/client/src/utils/date.ts
@@ -1,9 +1,11 @@
 import dayjs, { extend } from "dayjs";
+import dayjsCustomParseFormat from "dayjs/plugin/customParseFormat";
 import dayjsDevHelper from "dayjs/plugin/devHelper";
 import dayjsRelativeTime from "dayjs/plugin/relativeTime";
 import dayjsTimezone from "dayjs/plugin/timezone";
 import dayjsUtc from "dayjs/plugin/utc";
 
+extend(dayjsCustomParseFormat);
 extend(dayjsDevHelper);
 extend(dayjsUtc);
 extend(dayjsTimezone);
