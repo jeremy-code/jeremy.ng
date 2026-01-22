bc60a22eb5800685194db83be0d7c9c6586d5566
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-01-22 00:01:56 -0800

    fix: add temp fix for Tailwind bug where escaped classes generate incorrect CSS
    - Add temporary fix in <HorizontalList /> component in regards to a bug in Tailwind CSS where escaped JavaScript `className`s generate incorrect CSS. See tailwindlabs/tailwindcss#19589

--- packages/ui/components/HorizontalList.tsx
+++ packages/ui/components/HorizontalList.tsx
@@ -15,6 +15,13 @@ const HorizontalListItem = ({
 }: ComponentPropsWithRef<"li">) => {
   return (
     <li
+      /**
+       * The following comment is a minor fix for a bug in Tailwind CSS's CSS
+       * generation when using escaped characters.
+       *
+       * @see {@link https://github.com/tailwindlabs/tailwindcss/issues/19589 }
+       */
+      // className="not-last:after:content-['\a0_·_']"
       className={cn(
         "inline not-last:after:font-black not-last:after:content-['\\a0_·_']",
         className,
