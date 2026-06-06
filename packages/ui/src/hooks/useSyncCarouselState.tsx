6014b5d9e12a6c3a0c303a32ffbadbf540b1398d
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-06-05 19:48:13 -0700

    fix: fix hydration bug in useSyncCarouselState
    - Replace useSyncExternalStore with useEffect to avoid hydration bug in CarouselControls

--- packages/ui/src/hooks/useSyncCarouselState.tsx
+++ packages/ui/src/hooks/useSyncCarouselState.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useSyncExternalStore } from "react";
+import { useEffect, useEffectEvent, useState } from "react";
 
 import { type UseEmblaCarouselType } from "embla-carousel-react";
 
@@ -10,34 +10,31 @@ type CarouselState = {
 };
 
 const useSyncCarouselState = (api: UseEmblaCarouselType[1]) => {
-  const subscribe = useCallback(
-    (onStoreChange: () => void) => {
-      api?.on("select", onStoreChange);
-      api?.on("reInit", onStoreChange);
-      return () => {
-        api?.off("select", onStoreChange);
-        api?.off("reInit", onStoreChange);
-      };
+  const [canScrollPrev, setCanScrollPrev] = useState(false);
+  const [canScrollNext, setCanScrollNext] = useState(false);
+
+  const handler = useEffectEvent(
+    (emblaApi: Exclude<UseEmblaCarouselType[1], undefined>) => {
+      setCanScrollPrev(emblaApi.canScrollPrev());
+      setCanScrollNext(emblaApi.canScrollNext());
     },
-    [api],
   );
 
-  // Not memoizing `getSnapshot()` functions, https://github.com/pmndrs/zustand/discussions/971
-  const canScrollPrev = useSyncExternalStore(
-    subscribe,
-    () => api?.canScrollPrev() ?? false,
-    /**
-     * Having an undefined `getServerSnapshot()` gives the follow error in
-     * Next.js: "Missing getServerSnapshot, which is required for
-     * server-rendered content. Will revert to client rendering."
-     */
-    () => false,
-  );
-  const canScrollNext = useSyncExternalStore(
-    subscribe,
-    () => api?.canScrollNext() ?? false,
-    () => false,
-  );
+  useEffect(() => {
+    if (api === undefined) {
+      return;
+    }
+
+    api.on("init", handler);
+    api.on("select", handler);
+    api.on("reInit", handler);
+
+    return () => {
+      api.off("init", handler);
+      api.off("select", handler);
+      api.off("reInit", handler);
+    };
+  }, [api]);
 
   return {
     canScrollPrev,
