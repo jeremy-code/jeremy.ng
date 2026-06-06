ae867268508c3e41c22eac8a8a24a65db9a61be0
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-06-05 18:40:56 -0700

    chore: add alt to Navbar logo

--- apps/client/src/components/layout/Navbar.tsx
+++ apps/client/src/components/layout/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = ({ className, ...props }: ComponentPropsWithRef<"header">) => {
     <header className={cn("relative border-b", className)} {...props}>
       <div className="container flex items-center justify-between py-4">
         <Link className="flex items-center gap-2 font-semibold" to="/">
-          <img width="24" height="24" src="/favicon.svg" />
+          <img width="24" height="24" alt="Jeremy Nguyen" src="/favicon.svg" />
           Jeremy Nguyen
         </Link>
         <div className="flex items-center gap-2">
