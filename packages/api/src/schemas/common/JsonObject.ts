b68c8c0dd2c49a5d9bffe9a37d6c78fba50f9ecd
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-06-10 22:05:49 -0700

    fix: check JsonObject validates type definition

--- packages/api/src/schemas/common/JsonObject.ts
+++ packages/api/src/schemas/common/JsonObject.ts
@@ -1,5 +1,9 @@
+import type { JsonObject as IJsonObject } from "mailgun.js/definitions";
 import { z } from "zod";
 
-const JsonObject = z.record(z.string(), z.json());
+const JsonObject = z.record(
+  z.string(),
+  z.json(),
+) satisfies z.ZodType<IJsonObject>;
 
 export { JsonObject };
