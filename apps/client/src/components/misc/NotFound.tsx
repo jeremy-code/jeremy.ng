12ee9ba19d35bd793c24089ee2a00d74ee6f6be8
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-06-10 21:01:19 -0700

    refactor: improve NotFound component layout
    - Log errors on NotFound
    - Update layout to be one column
    - Update image sizing to be fullWidth instead of manually setting widths
    - Add "Go back" button

--- apps/client/src/components/misc/NotFound.tsx
+++ apps/client/src/components/misc/NotFound.tsx
@@ -1,28 +1,39 @@
 import { Link, type NotFoundRouteProps } from "@tanstack/react-router";
+import { ChevronLeft } from "lucide-react";
 
 import { Image } from "#components/common/Image";
 import { Button } from "@jeremyng/ui/components/Button";
 import { Heading } from "@jeremyng/ui/components/Heading";
 
-const NotFound = (_props: NotFoundRouteProps) => {
+const NotFound = (props: NotFoundRouteProps) => {
+  console.warn(
+    `Route ${props.routeId} was ${props.isNotFound ? "not found" : "found"} with data ${JSON.stringify(props.data)}`,
+  );
+
   return (
-    <main className="container grid grid-cols-1 items-center justify-items-center py-4 sm:grid-cols-2 sm:gap-8">
+    <main className="container flex flex-col justify-center gap-8 py-4">
       <Image
         alt="Illustration of a flying saucer beaming up a cyclopean alien while another alien watches from the ground."
-        className="h-80 max-sm:justify-self-center sm:h-100 sm:justify-self-end md:h-110 lg:h-125"
+        className="mx-auto max-w-[min(100%,var(--container-md))]"
         src="/assets/not-found.webp"
         layout="fullWidth"
       />
-      <div className="order-first flex flex-col items-start gap-2 justify-self-start sm:order-last">
-        <Heading as="h1" size="3xl">
+      <div className="flex flex-col items-start justify-center gap-2 sm:items-center">
+        <Heading as="h1" size="4xl">
           Not Found
         </Heading>
         <p className="text-fg-muted mb-4">
           The page you are looking for does not exist.
         </p>
-        <Button asChild>
-          <Link to="/">Go Home</Link>
-        </Button>
+        <div className="flex gap-2">
+          <Button asChild>
+            <Link to="/">Go Home</Link>
+          </Button>
+          <Button variant="solid" onClick={() => window.history.back()}>
+            <ChevronLeft size={16} />
+            Go Back
+          </Button>
+        </div>
       </div>
     </main>
   );
