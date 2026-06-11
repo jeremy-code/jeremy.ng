047d3d9957b4b7c6f2430bf14e4ad4aaeafb28eb
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-06-10 20:26:56 -0700

    feat: add Unpic Image component and use it with NotFound image
    - Add Unpic Image Component with cloudflare presets
    - Move not-found.svg to assets folder
    - Export not-found.svg to webp file
    - Replace NotFound with webp image

---
+++ apps/client/src/components/common/Image.tsx
@@ -0,0 +1,33 @@
+import type { ComponentPropsWithRef } from "react";
+
+import { Image as UnpicImage } from "@unpic/react";
+
+import { env } from "#config/env";
+
+type ImageProps = ComponentPropsWithRef<typeof UnpicImage>;
+
+const Image = (props: ImageProps) => {
+  return (
+    <UnpicImage
+      {...props}
+      // Default Unpic breakpoints and Tailwind CSS breakpoints
+      // 20rem, 40rem (sm), 48rem (md), 60rem (lg), 64rem, 80rem (xl), 96rem (2xl)
+      breakpoints={props.breakpoints ?? [320, 640, 768, 960, 1024, 1280, 1536]}
+      cdn={props.cdn ?? (import.meta.env.PROD ? "cloudflare" : undefined)}
+      fallback={
+        props.fallback ?? (import.meta.env.PROD ? "cloudflare" : undefined)
+      }
+      options={{
+        ...props.options,
+        ...(import.meta.env.PROD && {
+          cloudflare: {
+            ...props.options?.cloudflare,
+            domain: env.VITE_BASE_URL,
+          },
+        }),
+      }}
+    />
+  );
+};
+
+export { Image, type ImageProps };
--- apps/client/src/components/misc/NotFound.tsx
+++ apps/client/src/components/misc/NotFound.tsx
@@ -1,15 +1,17 @@
 import { Link, type NotFoundRouteProps } from "@tanstack/react-router";
 
+import { Image } from "#components/common/Image";
 import { Button } from "@jeremyng/ui/components/Button";
 import { Heading } from "@jeremyng/ui/components/Heading";
 
 const NotFound = (_props: NotFoundRouteProps) => {
   return (
     <main className="container grid grid-cols-1 items-center justify-items-center py-4 sm:grid-cols-2 sm:gap-8">
-      <img
+      <Image
         alt="Illustration of a flying saucer beaming up a cyclopean alien while another alien watches from the ground."
         className="h-80 max-sm:justify-self-center sm:h-100 sm:justify-self-end md:h-110 lg:h-125"
-        src="/not-found.svg"
+        src="/not-found.webp"
+        layout="fullWidth"
       />
       <div className="order-first flex flex-col items-start gap-2 justify-self-start sm:order-last">
         <Heading as="h1" size="3xl">
