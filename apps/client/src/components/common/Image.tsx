dbaf7acf976aa2b0e61924ceb1b35c3159ae6851
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-06-13 19:32:35 -0700

    feat(client): directly import cloudflare provider for unpic
    - Directly import Cloudflare provider for unpic for better treeshaking and decrease bundle size

--- apps/client/src/components/common/Image.tsx
+++ apps/client/src/components/common/Image.tsx
@@ -1,12 +1,34 @@
 import type { ComponentPropsWithRef } from "react";
 
-import { Image as UnpicImage } from "@unpic/react";
+import { Image as UnpicImage } from "@unpic/react/base";
+import type { TransformerFunction } from "unpic";
+import {
+  transform as cloudflareTransform,
+  type CloudflareOperations,
+  type CloudflareOptions,
+} from "unpic/providers/cloudflare";
 
 import { env } from "#config/env";
 
-type ImageProps = ComponentPropsWithRef<typeof UnpicImage>;
+// https://github.com/microsoft/TypeScript/issues/46361
+type DistributedOmit<T, K extends PropertyKey> =
+  T extends unknown ? Omit<T, K> : never;
+
+type ImageProps = DistributedOmit<
+  ComponentPropsWithRef<
+    typeof UnpicImage<CloudflareOperations, CloudflareOptions>
+  >,
+  "transformer"
+>;
 
 const CLOUDFLARE_DOMAIN = new URL(env.VITE_BASE_URL).host;
+
+const baseTransform: TransformerFunction<
+  CloudflareOperations,
+  CloudflareOptions
+> = (src) => (typeof src === "string" ? src : src.toString());
+
+const transform = import.meta.env.DEV ? baseTransform : cloudflareTransform;
 
 const Image = (props: ImageProps) => {
   return (
@@ -15,18 +37,10 @@ const Image = (props: ImageProps) => {
       // Default Unpic breakpoints and Tailwind CSS breakpoints
       // 20rem, 40rem (sm), 48rem (md), 60rem (lg), 64rem, 80rem (xl), 96rem (2xl)
       breakpoints={props.breakpoints ?? [320, 640, 768, 960, 1024, 1280, 1536]}
-      cdn={props.cdn ?? (import.meta.env.PROD ? "cloudflare" : undefined)}
-      fallback={
-        props.fallback ?? (import.meta.env.PROD ? "cloudflare" : undefined)
-      }
+      transformer={transform}
       options={{
+        domain: CLOUDFLARE_DOMAIN,
         ...props.options,
-        ...(import.meta.env.PROD && {
-          cloudflare: {
-            ...props.options?.cloudflare,
-            domain: CLOUDFLARE_DOMAIN,
-          },
-        }),
       }}
     />
   );
