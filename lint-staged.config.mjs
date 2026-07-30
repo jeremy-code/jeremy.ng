2100d38361b23c873970b5c76e4b9b78fbcc735f
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-07-29 23:14:47 -0700

    chore: update lint command to eslint in lint-staged.config.mjs

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -4,13 +4,7 @@
  * @satisfies {Configuration}
  */
 const lintStagedConfig = {
-  /**
-   * Prevents turbo from seeing the filenames as tasks and instead passes them
-   * as arguments.
-   *
-   * @see {@link https://turborepo.dev/docs/reference/run}
-   */
-  "*.{js,mjs,cjs,ts,tsx,mts,cts}": ["turbo run lint --", "prettier --check"],
+  "*.{js,mjs,cjs,ts,tsx,mts,cts}": ["eslint", "prettier --check"],
   "*.{json,md,yaml,yml}": "prettier --check",
 };
 
