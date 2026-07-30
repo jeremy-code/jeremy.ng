c7ffb1aa6242a0ffc34eb4ff399c883c4a250e16
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-07-29 19:41:13 -0700

    chore(api): replace contact email with hi instead of jeremy

--- packages/api/src/routers/contact.ts
+++ packages/api/src/routers/contact.ts
@@ -12,8 +12,8 @@ const contactRouter = createTRPCRouter({
       return sendMessage({
         domain: env.MAILGUN_DOMAIN,
         data: {
-          from: `Jeremy <jeremy@${env.MAILGUN_DOMAIN}>`,
-          to: `Jeremy <jeremy@${env.MAILGUN_DOMAIN}>`,
+          from: `Jeremy <hi@${env.MAILGUN_DOMAIN}>`,
+          to: `Jeremy <hi@${env.MAILGUN_DOMAIN}>`,
           cc: `${opts.input.name} <${opts.input.email}>`,
           subject: "New message from contact form on jeremy.ng",
           text: opts.input.message,
