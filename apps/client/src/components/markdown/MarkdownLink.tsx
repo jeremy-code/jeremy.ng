8a16764ec8570d81f64c786c6006e431bd825893
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-08-01 20:06:56 -0700

    fix(client): update MarkdownLink logic to work with asset URLs
    - Fix MarkdownLink logic so that it checks if it is a file in /blog/*.md,
      otherwise use a normal link, to handle asset urls /blog/asset/*

--- apps/client/src/components/markdown/MarkdownLink.tsx
+++ apps/client/src/components/markdown/MarkdownLink.tsx
@@ -1,4 +1,4 @@
-import { basename, extname } from "node:path";
+import { normalize, parse } from "node:path";
 import type { ComponentPropsWithRef } from "react";
 
 import { Link as RouterLink } from "@tanstack/react-router";
@@ -6,25 +6,31 @@ import { Link as RouterLink } from "@tanstack/react-router";
 import { Link, linkVariants } from "@jeremyng/ui/components/Link";
 
 const MarkdownLink = ({ href, ...props }: ComponentPropsWithRef<"a">) => {
-  // Hash or Absolute URL
+  // Undefined, hash, or absolute URL (i.e. has protocol)
   if (href === undefined || href?.startsWith("#") || URL.canParse(href)) {
     return <Link {...props} color="link" underline="hover" href={href} />;
   }
 
-  return (
-    <RouterLink
-      {...props}
-      className={linkVariants({ color: "link", underline: "hover" })}
-      to="/blog/$slug"
-      // Can't use function because otherwise
-      // Server Error: Functions cannot be passed directly to Client Components unless you explicitly expose it by m arking it with "use server". Or maybe you meant to call this function rather than return it.
-      params={{
-        slug: basename(href, extname(href)),
-      }}
-    >
-      {props.children}
-    </RouterLink>
-  );
+  const parsedNormalizedHref = parse(normalize(href));
+
+  // Relative URL to .md file in the same directory
+  if (parsedNormalizedHref.ext === ".md" && parsedNormalizedHref.dir === "") {
+    return (
+      // Looks like using Link from #components/common results in
+      // Error: Unexpectedly client reference export 'createLink' is called on server
+      <RouterLink
+        {...props}
+        className={linkVariants({ color: "link", underline: "hover" })}
+        to="/blog/$slug"
+        params={{
+          slug: parsedNormalizedHref.name,
+        }}
+      />
+    );
+  }
+
+  // Relative URL (possibly asset)
+  return <Link {...props} color="link" underline="hover" href={href} />;
 };
 
 export { MarkdownLink };
