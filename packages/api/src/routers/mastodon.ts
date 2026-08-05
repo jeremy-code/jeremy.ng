c2c47c749521249048d8f354cc0df56a96ec079b
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-08-05 00:28:33 -0700

    chore(api): update error handling in getStatus procedure in mastodon router

--- packages/api/src/routers/mastodon.ts
+++ packages/api/src/routers/mastodon.ts
@@ -1,5 +1,5 @@
 import { TRPCError } from "@trpc/server";
-import { FetchError, ofetch } from "ofetch";
+import { ofetch } from "ofetch";
 import * as z from "zod";
 
 import { statusSchema, type Status } from "../schemas/mastodon/status";
@@ -15,35 +15,30 @@ const mastodonRouter = createTRPCRouter({
     .input(z.strictObject({ statusId: z.string().trim() }))
     .output(statusSchema)
     .query(async (opts) => {
-      try {
-        const response = await mastodonApi<Status>(
-          `/api/v1/statuses/${opts.input.statusId}`,
-          {
-            method: "GET",
-            query: opts.input,
+      const response = await mastodonApi<Status>(
+        `/api/v1/statuses/${opts.input.statusId}`,
+        {
+          method: "GET",
+          onResponseError: ({ response, error }) => {
+            if (response.status === 401) {
+              throw new TRPCError({
+                message:
+                  "Mastodon server required authorization to fetch status.",
+                code: "INTERNAL_SERVER_ERROR",
+                cause: error,
+              });
+            } else if (response.status === 404) {
+              throw new TRPCError({
+                message: "The status could not be found.",
+                code: "NOT_FOUND",
+                cause: error,
+              });
+            }
           },
-        );
+        },
+      );
 
-        return response;
-      } catch (e) {
-        if (e instanceof FetchError) {
-          if (e.statusCode === 401) {
-            throw new TRPCError({
-              message:
-                "Mastodon server required authorization to fetch status.",
-              code: "INTERNAL_SERVER_ERROR",
-              cause: e,
-            });
-          } else if (e.statusCode === 404) {
-            throw new TRPCError({
-              message: "The status could not be found.",
-              code: "NOT_FOUND",
-              cause: e,
-            });
-          }
-        }
-        throw e;
-      }
+      return response;
     }),
 });
 
